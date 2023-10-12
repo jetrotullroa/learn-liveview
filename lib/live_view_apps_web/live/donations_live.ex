63c53f5cba8cd1a353041a336dd24cee92e70633
Jetro Tull Roa <jetrotullroa@gmail.com>
2023-10-12 18:58:22 -0400

    Sorting Feature

--- lib/live_view_apps_web/live/donations_live.ex
+++ lib/live_view_apps_web/live/donations_live.ex
@@ -4,13 +4,35 @@ defmodule LiveViewAppsWeb.DonationsLive do
   alias LiveViewApps.Donations
 
   def mount(_params, _session, socket) do
-    donations = Donations.list_donations()
-
-    socket =
-      assign(socket,
-        donations: donations
-      )
-
     {:ok, socket}
+  end
+
+  def handle_params(params, _uri, socket) do
+    sort_by = (params["sort_by"] || "id") |> String.to_atom()
+    sort_order = (params["sort_order"] || "asc") |> String.to_atom()
+
+    options = %{sort_by: sort_by, sort_order: sort_order}
+    donations = Donations.list_donations(options)
+
+    socket = assign(socket, donations: donations, options: options)
+
+    {:noreply, socket}
+  end
+
+  defp sort_item_link(assigns) do
+    ~H"""
+    <.link patch={
+      ~p"/donations?#{%{sort_by: @sort_by, sort_order: next_sort_order(@options.sort_order)}}"
+    }>
+      <%= render_slot(@inner_block) %>
+    </.link>
+    """
+  end
+
+  defp next_sort_order(sort_order) do
+    case sort_order do
+      :asc -> :desc
+      :desc -> :asc
+    end
   end
 end
