13a6f5dec47770fe6003cb1bd4533fc0f7194c86
Jetro Tull Roa <jetrotullroa@gmail.com>
2023-10-21 18:47:21 -0400

    Live Ecto Forms and Lists

--- lib/live_view_apps_web/live/volunteers_live.ex
+++ lib/live_view_apps_web/live/volunteers_live.ex
@@ -5,10 +5,13 @@ defmodule LiveViewAppsWeb.VolunteersLive do
 
   def mount(_params, _session, socket) do
     volunteers = Volunteers.list_volunteers()
+    changeset = Volunteers.change_volunteer(%Volunteers.Volunteer{})
+    form = to_form(changeset)
 
     socket =
       assign(socket,
-        volunteers: volunteers
+        volunteers: volunteers,
+        form: form
       )
 
     {:ok, socket}
@@ -18,6 +21,20 @@ defmodule LiveViewAppsWeb.VolunteersLive do
     ~H"""
     <h1>Volunteer Check-In</h1>
     <div id="volunteer-checkin">
+      <.form for={@form} phx-submit="save">
+        <.input
+          field={@form[:name]}
+          placeholder="Your name here..."
+          autocomplete="off"
+        />
+        <.input
+          type="tel"
+          field={@form[:phone]}
+          placeholder="434-23-31"
+          autocomplete="off"
+        />
+        <.button phx-disable-with="Submitting...">Check In</.button>
+      </.form>
       <div
         :for={volunteer <- @volunteers}
         class={"volunteer #{if volunteer.checked_out, do: "out"}"}
@@ -36,5 +53,19 @@ defmodule LiveViewAppsWeb.VolunteersLive do
       </div>
     </div>
     """
+  end
+
+  def handle_event("save", %{"volunteer" => volunteer_params}, socket) do
+    case Volunteers.create_volunteer(volunteer_params) do
+      {:ok, volunteer} ->
+        {:noreply,
+         assign(socket,
+           volunteers: [volunteer | socket.assigns.volunteers],
+           form: to_form(Volunteers.change_volunteer(%Volunteers.Volunteer{}))
+         )}
+
+      {:error, changeset} ->
+        {:noreply, assign(socket, form: to_form(changeset))}
+    end
   end
 end
